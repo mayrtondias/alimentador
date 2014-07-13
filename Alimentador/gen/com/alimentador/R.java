eedb10aa6eb4d112f1f9a3676a7aa1bd16272534
mayrtondias <mayrtondias@hotmail.com>
2014-07-12 22:39:11 -0300

    update

--- Alimentador/gen/com/alimentador/R.java
+++ Alimentador/gen/com/alimentador/R.java
@@ -995,13 +995,18 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050045;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int buttonDate=0x7f050044;
+        public static final int buttonHora=0x7f050043;
+        public static final int buttonOkData=0x7f05003f;
+        public static final int buttonOkHora=0x7f050042;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
+        public static final int datePicker1=0x7f05003e;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1041,6 +1046,9 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textInfoData=0x7f05003d;
+        public static final int textInfoHora=0x7f050040;
+        public static final int timePicker1=0x7f050041;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1096,17 +1104,25 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_inicial=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int fragment_inicial=0x7f030019;
-        public static final int fragment_main=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int activity_data=0x7f030017;
+        public static final int activity_hora=0x7f030018;
+        public static final int activity_inicial=0x7f030019;
+        public static final int activity_main=0x7f03001a;
+        public static final int fragment_data=0x7f03001b;
+        public static final int fragment_hora=0x7f03001c;
+        public static final int fragment_inicial=0x7f03001d;
+        public static final int fragment_main=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
     }
     public static final class menu {
-        public static final int inicial=0x7f0c0000;
-        public static final int main=0x7f0c0001;
+        public static final int data=0x7f0c0000;
+        public static final int hora=0x7f0c0001;
+        public static final int inicial=0x7f0c0002;
+        public static final int main=0x7f0c0003;
     }
     public static final class string {
+        public static final int InfData=0x7f0a0011;
+        public static final int InfHora=0x7f0a0010;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1147,10 +1163,15 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_settings=0x7f0a0014;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
-        public static final int title_activity_inicial=0x7f0a0010;
+        public static final int data=0x7f0a000f;
+        public static final int hello_world=0x7f0a0012;
+        public static final int hora=0x7f0a000e;
+        public static final int ok=0x7f0a0013;
+        public static final int title_activity_data=0x7f0a0017;
+        public static final int title_activity_hora=0x7f0a0016;
+        public static final int title_activity_inicial=0x7f0a0015;
     }
     public static final class style {
         /** 
